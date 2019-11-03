6712f899abd96ddf870562418113d07cd2ed30f9
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-11-03 15:18:03 -0500

    added the functions for calling api and database

--- create-react-express/client/src/utils/Api.js
+++ create-react-express/client/src/utils/Api.js
@@ -1,17 +1,30 @@
 import axios from 'axios';
+import db from '../../../models';
 
 export default  {
-    getBooks = () => {
+    getBooks : () => {
+      db.Books.find({},(err,data) =>{
+          if (err) {
+            
+          } else {
+            
+          }
+
+      })
+    },
+    deleteBook : (id) => {
 
     },
-    deleteBook = (id) => {
+    saveBook : (id) => {
+        db.Books.insertOne({
+
+        })
 
     },
-    saveBook = (id) => {
-
-    },
-    requestBook = (query) => {
-
+    requestBook : (query) => {
+         axios.get('https://www.googleapis.com/books/v1/volumes?q='+query+'&key=AIzaSyBYrSwBkmTj5gzN4NObWjt4z7BMmsA6uUk').then((data)=>{
+             console.log(data)
+         })
     }
 
 }
