d958dd1b67c2182b8d7c5dd3478f254ef98650f5
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-11-04 13:55:38 -0500

    added api-routes to the api-routes folder

--- create-react-express/routes/api-routes.js
+++ create-react-express/routes/api-routes.js
@@ -3,10 +3,38 @@ const db = require('../models')
 const path = require('path');
 
 module.exports = (app) => {
-    app.get('./api/Books', function(req,res){
-        db.Books.find().then(function(data) {
-             res.json(data)
+    app.get('/api/Books', function (req, res) {
+        db.Books.find({}).then((err, data) => {
+            if (err) {
+                res.sendStatus(500)
+            } else {
+                res.sendStatus(200)
+                res.sendStatus(data)
+            }
+        })
+    });
+    app.post('/api/books', (req, res) => {
+        db.Books.insertOne({
+
+        }).then((err, data) => {
+            if (err) {
+                res.sendStatus(500)
+            } else {
+                res.sendStatus(200)
+            }
+        })
+
+    });
+    app.delete('api/books/:id', (req, res) => {
+        const id = req.params.id
+        db.Books.destroy({
+            id: id
+        }).then((err, data) => {
+            if (err) {
+               res.sendStatus(500)
+            } else {
+               res.sendStatus(200)
+            }
         })
     })
-    app.post()
 }
